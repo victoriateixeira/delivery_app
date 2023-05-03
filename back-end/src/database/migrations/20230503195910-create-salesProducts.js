edc210164e66e0d8b92100482393b790e75ab598
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-05-03 17:02:25 -0300

    feat: cria migration para SALES PRODUCTS

---
+++ back-end/src/database/migrations/20230503195910-create-salesProducts.js
@@ -0,0 +1,42 @@
+/**
+* @param {import('sequelize').Sequelize} sequelize
+* @param {import('sequelize').DataTypes} sequelize.DataTypes
+*/
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('sales_products', {
+      saleId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'sale_id',
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references:{
+          model: 'sales',
+          key: 'id',
+        }
+      },
+      productId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'product_id',
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references:{
+          model: 'products',
+          key: 'id',
+      },
+    },
+      quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+   
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('sales_products');
+  }
+};
