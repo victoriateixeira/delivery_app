0c1ef94289832814fcc2348e7d010b840d1b72ac
André Pestana Silva <atlas@pop-os.localdomain>
2023-05-04 16:55:52 -0300

    feat: configuração de rota do React

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { Route, Switch } from 'react-router-dom';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-    </div>
+    <Switch>
+      <Route path="/register" component={ Register } exact />
+    </Switch>
   );
 }
 
