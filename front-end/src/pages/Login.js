efca382a6ab1e8855a77377c7ee64a2d5a5bf7e0
Marinna Vitoriano <marinna@outlook.com>
2023-05-04 17:19:52 -0300

    feat: implementa rota de login

---
+++ front-end/src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Login() {
+  return (
+    <main className="login-container">
+      <div className="logo-container">
+        {/* <img alt="app-logo" /> */}
+        <h1>NOME DO APP</h1>
+      </div>
+
+    </main>
+  );
+}
