eca4a704018755570f7e56f0eb11d577257b3601
joanamds <joanamdsantos1@gmail.com>
2023-05-04 16:55:28 -0300

    feat: criando o componente order card para renderizar os pedidos

---
+++ front-end/src/components/OrderCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function OrderCard({ id, date, status, price }) {
+  return (
+    <div className="order-card">
+      <p data-testid={ `customer_orders__element-order-id-${id}` }>{ id }</p>
+      <p data-testid={ `customer_orders__element-order-status-${id}` }> { status } </p>
+      <p data-testid={ `customer_orders__element-order-date-${id}` }>{ date }</p>
+      <p data-testid={ `customer_orders__element-order-price-${id}` }>{ price }</p>
+    </div>
+  );
+}
--- front-end/src/pages/CustomersOrders.js
+++ front-end/src/pages/CustomersOrders.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function CustomersOrders() {
-  return(
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    setOrders([{
+      id: 1,
+      date: new Date(),
+      status: 'pendente',
+      price: 1.56
+    }])
+  });
+
+  return (
     <div className="customers-orders">
+      {
+        orders.map((order) => {
+          <OrderCard
+            id={ order.id }
+            date={ order.date }
+            status={ order.status }
+            price={ order.price }
+          />
+        })
+      }
     </div>
   );
 }
