698358336aa096ebbe6cea0a81a34535af65c0dd
joanamds <joanamdsantos1@gmail.com>
2023-05-05 13:27:28 -0300

    fix: consertando as migrations

--- back-end/src/database/models/Product.js
+++ back-end/src/database/models/Product.js
@@ -1,16 +1,18 @@
 module.exports = (sequelize, DataTypes) => {
-  const Product = sequelize.define('Product',
-   {
-    id: { type: DataTypes.INTEGER, primaryKey: true }, 
-    name: DataTypes.STRING,
-    price: DataTypes.DECIMAL(9, 2),
-    url_image:DataTypes.STRING,
-  },
-  {
-    timestamps: false,
-    underscored: true,
-  },
+  const Product = sequelize.define(
+    'Product',
+    {
+      id: { type: DataTypes.INTEGER, primaryKey: true }, 
+      name: DataTypes.STRING,
+      price: DataTypes.DECIMAL(9, 2),
+      urlImage: DataTypes.STRING,
+    },
+    {
+      timestamps: false,
+      underscored: true,
+      tableName: 'products',
+    },
   );
- 
+
   return Product;
-}
+};
