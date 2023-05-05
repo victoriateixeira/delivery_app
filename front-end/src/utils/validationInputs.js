d5d2ab33d8ab0064e94d947bead0b77b334bb0fd
André Pestana Silva <atlas@pop-os.localdomain>
2023-05-05 15:02:26 -0300

    fix: validação login

--- front-end/src/utils/validationInputs.js
+++ front-end/src/utils/validationInputs.js
@@ -3,7 +3,7 @@ const passwordLenght = 6;
 const validationInputs = (email, password) => {
   const emailRegex = /\S+@\S+\.\S+/;
   const validationEmail = emailRegex.test(email);
-  const validationGeneral = validationEmail && password.length > passwordLenght;
+  const validationGeneral = validationEmail && password.length >= passwordLenght;
   return validationGeneral;
 };
 
