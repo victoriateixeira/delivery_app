f0eea96c46fcba866cec9ba422d1daf5e8667f52
André Pestana Silva <atlas@pop-os.localdomain>
2023-05-05 15:55:15 -0300

    feat: cria todos os elementos de Register

--- front-end/src/pages/Register.jsx
+++ front-end/src/pages/Register.jsx
@@ -2,7 +2,50 @@ import React from 'react';
 
 function Register() {
   return (
-    <h1>Register</h1>
+    <main className="register-container">
+      <h1>Cadastro</h1>
+      <form>
+        <label htmlFor="nome">
+          Nome
+          <input
+            type="text"
+            name="nome"
+            placeholder="Seu nome"
+            data-testid="common_register__input-name"
+          />
+        </label>
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            name="email"
+            placeholder="seu-email@site.com.br"
+            data-testid="common_register__input-email"
+          />
+        </label>
+        <label htmlFor="password">
+          Senha
+          <input
+            type="password"
+            name="password"
+            placeholder="******************"
+            data-testid="common_register__input-password"
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="common_register__button-register"
+        >
+          Cadastrar
+        </button>
+      </form>
+      <p
+        id="form-invalid-text"
+        hidden
+      >
+        Elemento oculto (Mensagem de erro)
+      </p>
+    </main>
   );
 }
 
