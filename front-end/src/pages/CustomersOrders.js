b275becbc1bdccba45aa660d70401e9ee316ac4c
joanamds <joanamdsantos1@gmail.com>
2023-05-08 18:47:17 -0300

    feat: comentando o mock do localstorage

--- front-end/src/pages/CustomersOrders.js
+++ front-end/src/pages/CustomersOrders.js
@@ -1,34 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import OrderCard from '../components/OrderCard';
 import requestAPI from '../services/deliveryAPI';
-import { saveUser, readUser } from '../services/localStorage';
+import { readUser } from '../services/localStorage';
 
 function CustomersOrders() {
   const [orders, setOrders] = useState([]);
 
   useEffect(() => {
-    saveUser({
-      id: 1,
-      name: 'Nome Da Pessoa Usuária',
-      email: 'email@dominio.com',
-      role: 'customer',
-      token: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
-      .eyJuYW1lIjoiTm9tZSBEYSBQZXNzb2EgVXN1w6FyaWEiLCJl
-      bWFpbCI6ImVtYWlsQGRvbWluaW8uY29tIiwicm9sZSI6ImN1c3RvbWVyIn0
-      .s5cmiyY16yViCXkHuzWekxkMeYBi75eT8uJnSbfadNE`,
-    });
+    // saveUser({
+    //   id: 1,
+    //   name: 'Nome Da Pessoa Usuária',
+    //   email: 'email@dominio.com',
+    //   role: 'customer',
+    //   token: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
+    //   .eyJuYW1lIjoiTm9tZSBEYSBQZXNzb2EgVXN1w6FyaWEiLCJl
+    //   bWFpbCI6ImVtYWlsQGRvbWluaW8uY29tIiwicm9sZSI6ImN1c3RvbWVyIn0
+    //   .s5cmiyY16yViCXkHuzWekxkMeYBi75eT8uJnSbfadNE`,
+    // });
     const { id } = readUser();
     const fetchOrders = async () => {
       const getOrders = await requestAPI(`/customers/orders/${id}`);
       setOrders(getOrders);
     };
     fetchOrders();
-    // setOrders([{
-    //   id: 1,
-    //   date: new Date().toLocaleDateString(),
-    //   status: 'pendente',
-    //   price: 1.56
-    // }])
   }, []);
 
   return (
