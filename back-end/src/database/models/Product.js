13d0a43ece4c122fc063cd8e9033234e9289516c
André Pestana Silva <atlas@pop-os.localdomain>
2023-05-08 18:41:18 -0300

    fix: arruma auto incremento id da Model Product

--- back-end/src/database/models/Product.js
+++ back-end/src/database/models/Product.js
@@ -2,7 +2,7 @@ module.exports = (sequelize, DataTypes) => {
   const Product = sequelize.define(
     'Product',
     {
-      id: { type: DataTypes.INTEGER, primaryKey: true }, 
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true }, 
       name: DataTypes.STRING,
       price: DataTypes.DECIMAL(9, 2),
       urlImage: DataTypes.STRING,
