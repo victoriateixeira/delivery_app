a8a8d967b5b87b988f9ebda699b05d75ea4a52a1
André Pestana Silva <atlas@pop-os.localdomain>
2023-05-08 14:08:16 -0300

    refactor: corrigindo erros de lint da model User

--- back-end/src/database/models/User.js
+++ back-end/src/database/models/User.js
@@ -1,24 +1,24 @@
-module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User',
-   {
-id: { type: DataTypes.INTEGER, primaryKey: true }, 
-name: DataTypes.STRING,
-email: DataTypes.STRING,
-password:DataTypes.STRING,
-role:DataTypes.STRING,
-  },
-  {
-    timestamps: false,
-    underscored: true,
-  },
+const defineUser = (sequelize, DataTypes) => sequelize.define(
+  'User',
+    {
+      id: { type: DataTypes.INTEGER, primaryKey: true }, 
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      role: DataTypes.STRING,
+    },
+    { timestamps: false, underscored: true, tableName: 'users' },
   );
+
+module.exports = (sequelize, DataTypes) => {
+  const User = defineUser(sequelize, DataTypes);
   User.associate = (models) => {
-    User.hasMany(models.Sales,
-     { foreignKey: 'userId', as: 'userSales' });
-  User.associate = (models) => {
-    User.hasMany(models.Sales,
-     { foreignKey: 'sellerId', as: 'sellerSales' });
- };
+    User.hasMany(
+      models.Sale,
+      { foreignKey: 'userId', as: 'userSales' },
+      { foreignKey: 'sellerId', as: 'seller' },
+    );
+  };
+
   return User;
-}
-}
+};
