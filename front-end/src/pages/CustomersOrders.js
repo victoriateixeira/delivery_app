fca1e66f5ae3c0eea2ce420be1428dfd2c15c921
joanamds <joanamdsantos1@gmail.com>
2023-05-09 10:58:04 -0300

    fix: consertando e criando função para login

--- front-end/src/pages/CustomersOrders.js
+++ front-end/src/pages/CustomersOrders.js
@@ -1,22 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import OrderCard from '../components/OrderCard';
-import requestAPI from '../services/deliveryAPI';
-import { readUser } from '../services/localStorage';
+import { requestAPI } from '../services/deliveryAPI';
+import { saveUser, readUser } from '../services/localStorage';
 
 function CustomersOrders() {
   const [orders, setOrders] = useState([]);
 
   useEffect(() => {
-    // saveUser({
-    //   id: 1,
-    //   name: 'Nome Da Pessoa Usuária',
-    //   email: 'email@dominio.com',
-    //   role: 'customer',
-    //   token: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
-    //   .eyJuYW1lIjoiTm9tZSBEYSBQZXNzb2EgVXN1w6FyaWEiLCJl
-    //   bWFpbCI6ImVtYWlsQGRvbWluaW8uY29tIiwicm9sZSI6ImN1c3RvbWVyIn0
-    //   .s5cmiyY16yViCXkHuzWekxkMeYBi75eT8uJnSbfadNE`,
-    // });
+    saveUser({
+      id: 1,
+      name: 'Nome Da Pessoa Usuária',
+      email: 'email@dominio.com',
+      role: 'customer',
+      token: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
+      .eyJuYW1lIjoiTm9tZSBEYSBQZXNzb2EgVXN1w6FyaWEiLCJl
+      bWFpbCI6ImVtYWlsQGRvbWluaW8uY29tIiwicm9sZSI6ImN1c3RvbWVyIn0
+      .s5cmiyY16yViCXkHuzWekxkMeYBi75eT8uJnSbfadNE`,
+    });
     const { id } = readUser();
     const fetchOrders = async () => {
       const getOrders = await requestAPI(`/customers/orders/${id}`);
--- front-end/src/services/deliveryAPI.js
+++ front-end/src/services/deliveryAPI.js
@@ -4,8 +4,14 @@ const api = axios.create({
   baseURL: `http://localhost:${process.env.REACT_APP_API_PORT || '3001'}`,
 });
 
-export default requestAPI = async (endpoint) => {
+export const requestAPI = async (endpoint) => {
   const { data } = await api.get(`${endpoint}`);
+
+  return data;
+};
+
+export const login = async (endpoint, body) => {
+  const data = await api.post(`${endpoint}`, body);
 
   return data;
 };
