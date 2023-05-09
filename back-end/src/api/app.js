fe44879e1b9007f7c3ad462f7c5b44957b6c344c
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-05-09 17:32:27 -0300

    fix: adicionar a importacão de cors ao arquivo app e corrige rota de products

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const { productRouter } = require('../routes');
 const errorMiddleware = require('../controllers/middlewares/errorMiddleware');
 
 const app = express();
 app.use(express.json());
-app.use('/products', productRouter);
+app.use(cors());
+app.use('/customer/products', productRouter);
 app.use(errorMiddleware);
 app.get('/coffee', (_req, res) => res.status(418).end());
 
