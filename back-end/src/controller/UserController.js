c50061054e3873fd124ac3b5fb2b9522cc74da97
André Pestana Silva <atlas@pop-os.localdomain>
2023-05-09 15:24:30 -0300

    refactor: adotando padrão arrow function

--- back-end/src/controller/UserController.js
+++ back-end/src/controller/UserController.js
@@ -1,10 +1,10 @@
 const userService = require('../service/UserService');
 
-async function createUser(req, res) {
+const createUser = async (req, res) => {
   const user = req.body;
   const result = await userService.createUser(user);
   res.status(201).json(result);
-}
+};
 
 module.exports = {
   createUser,
--- back-end/src/service/UserService.js
+++ back-end/src/service/UserService.js
@@ -1,17 +1,17 @@
 const md5 = require('md5');
 const { User } = require('../database/models');
 
-async function findAll() {
+const findAll = async () => {
   const result = await User.findAll();
   return result;
-}
+};
 
-async function createUser(user) {
+const createUser = async (user) => {
   const newUser = user;
   newUser.password = md5(user.password);
   const result = await User.create(newUser);
   return result;
-}
+};
 
 module.exports = {
   findAll,
