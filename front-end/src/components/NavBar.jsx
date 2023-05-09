5c833fd5a91380eaad0468dc26fa116dd4d7f5b6
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-05-09 17:33:49 -0300

    fix: corrige chamada da função handlesClick em NavBar

--- front-end/src/components/NavBar.jsx
+++ front-end/src/components/NavBar.jsx
@@ -1,51 +1,52 @@
-import React, { useEffect } from 'react';
-import {useHistory} from 'react-router';
+import React from 'react';
+import { useHistory } from 'react-router';
+
 function NavBar() {
   // useEffect() => {
   //   //Resgatar o NOME do usuário do localStorage
   // }
 
-const history = useHistory();
-const handlesClick = (page) => {
-  history.push(`/${page}`)
-};
+  const history = useHistory();
+  const handlesClick = (page) => {
+    history.push(`/${page}`);
+  };
   return (
-   
+
     <nav>
       <ul>
         <li>
-        <button 
-  data-testid='customer_products__element-navbar-link-products'
-  type= 'button'
-  onClick={handlesClick('products')}
-  >
-    Produtos
-    </button>
+          <button
+            data-testid="customer_products__element-navbar-link-products"
+            type="button"
+            onClick={ () => handlesClick('customer/products') }
+          >
+            Produtos
+          </button>
         </li>
         <li>
-        <button 
-  data-testid='customer_products__element-navbar-link-orders'
-  type= 'button'
-  onClick={handlesClick('orders')}
-  
-  >
-    Meus Pedidos
-    </button>
+          <button
+            data-testid="customer_products__element-navbar-link-orders"
+            type="button"
+            onClick={ () => handlesClick('customer/orders') }
+          >
+            Meus Pedidos
+          </button>
         </li>
         <li
-        data-testid='customer_products__element-navbar-user-full-name'
-        type= 'button'
-        onClick={handlesClick('products')}
+          data-testid="customer_products__element-navbar-user-full-name"
+          // type="button"
+          // onClick={ () => handlesClick('customer/products') }
         >
-NOME DO USUÁRIO
+          NOME DO USUÁRIO
         </li>
         <li>
-        <button 
-  data-testid ='customer_products__element-navbar-link-logout'
-  type= 'button'
-  onClick={handlesClick('login')}>
-    Sair
-    </button>
+          <button
+            data-testid="customer_products__element-navbar-link-logout"
+            type="button"
+            // onClick={ () => handlesClick('login') }
+          >
+            Sair
+          </button>
         </li>
       </ul>
     </nav>
