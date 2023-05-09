440f4f047488d566fd4e64e188827baa5561741f
joanamds <joanamdsantos1@gmail.com>
2023-05-09 17:39:52 -0300

    feat: atualizando branch

--- front-end/src/components/OrderCard.jsx
+++ front-end/src/components/OrderCard.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 function OrderCard({ id, date, status, price }) {
   return (
-    <div className="order-card">
+    <div data-testid={ `customer_products__element-order-date-${id}` }>
       <p data-testid={ `customer_orders__element-order-id-${id}` }>
         { id }
       </p>
