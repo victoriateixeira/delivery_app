d88cb6a5df20dfe3be3c03776631ecb8ff9d3db1
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-05-09 17:36:15 -0300

    fix: adiciona propTypes à ProductCard

--- front-end/src/components/ProductCard.jsx
+++ front-end/src/components/ProductCard.jsx
@@ -1,26 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-function ProductCard({product}) {
-  const {id, name, price, urlImage} = product;
-    return (
+function ProductCard({ product }) {
+  const { id, name, price, urlImage } = product;
+  return (
     <div
-    data-testid={`customer_products__element-card-price-${id}`}
+      data-testid={ `customer_products__element-card-price-${id}` }
     >
-      <Link to ={`/products/${id}`}>
-      <div> {price}</div>
-         <button
-    type= 'button'>
-      <div>
-        {name}
-      </div>
-      <img
-      src = {urlImage}
-      alt={name}>
-      </img>    
-      </button>
+      <Link to={ `customer/products/${id}` }>
+        <div>
+          R$
+          {price}
+        </div>
+        <button
+          type="button"
+        >
+          <div>
+            {name}
+          </div>
+          <img
+            src={ urlImage }
+            alt={ name }
+          />
+        </button>
       </Link>
     </div>
- 
-  )
+
+  );
 }
+ProductCard.propTypes = {
+  product: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    price: PropTypes.string.isRequired,
+    urlImage: PropTypes.string.isRequired,
+  }).isRequired,
+
+};
 export default ProductCard;
