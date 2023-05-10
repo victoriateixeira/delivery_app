25c8587b92a4872eb306e0732d92adeb4fa572bd
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-05-10 15:05:40 -0300

    feat: adiciona datatest-ids aos elementos do componente ProductCard

--- front-end/src/components/ProductCard.jsx
+++ front-end/src/components/ProductCard.jsx
@@ -1,30 +1,64 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import ProductContext from '../context/ProductContext';
 
 function ProductCard({ product }) {
+  const { cart, addToCart, removeFromCart } = useContext(ProductContext);
   const { id, name, price, urlImage } = product;
+  const [cartQty, setCartQty] = useState(0);
+  useEffect(() => {
+    setCartQty(cart.filter((cartItem) => cartItem.id === id).qty);
+  }, [cartQty, cart, id]);
+
   return (
     <div
       data-testid={ `customer_products__element-card-price-${id}` }
     >
       <Link to={ `customer/products/${id}` }>
-        <div>
+        <div
+          data-testid={ `customer_products__element-card-price-${id}` }
+        >
           R$
           {price}
         </div>
         <button
           type="button"
         >
-          <div>
+          <div
+            data-testid={ `customer_products__element-card-title-${id}` }
+          >
             {name}
           </div>
           <img
+            data-testid={ `customer_products__img-card-bg-image-${id}` }
             src={ urlImage }
             alt={ name }
           />
         </button>
       </Link>
+      <button
+        data-testid={ `customer_products__button-card-rm-item-${id}` }
+        type="button"
+        onClick={ () => removeFromCart(product) }
+        name="subtract"
+      >
+        -
+      </button>
+      <span
+        data-testid={ `customer_products__input-card-quantity-${id}` }
+      >
+        {cartQty || 0}
+
+      </span>
+      <button
+        data-testid={ `customer_products__button-card-add-item-${id}` }
+        type="button"
+        onClick={ () => addToCart(product) }
+        name="add"
+      >
+        +
+      </button>
     </div>
 
   );
