6269d00eb407f3f13fe8d4027c7df3ff5ab02bf8
joanamds <joanamdsantos1@gmail.com>
2023-05-10 12:06:20 -0300

    feat: criando a rota details

--- front-end/src/Routes.js
+++ front-end/src/Routes.js
@@ -2,6 +2,7 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import CustomersOrders from './pages/CustomersOrders';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import CustomersOrdersDetails from './pages/CustomersOrdersDetails';
 
 function Routes() {
   return (
@@ -11,6 +12,7 @@ function Routes() {
       <Route path="/register" component={ Register } exact />
       <Route path="/customer/products" exact />
       <Route path="/customers/orders" component={ CustomersOrders } exact />
+      <Route path="/customers/orders/:id" component={ CustomersOrdersDetails } exact />
     </Switch>
   );
 }
