34a852a3211ea509047b15fce15d58103cf13ba6
André Pestana Silva <atlas.ifpa@gmail.com>
2023-05-15 11:54:40 -0300

    refactor: removendo try Catch de UserController

--- back-end/src/controllers/UserController.js
+++ back-end/src/controllers/UserController.js
@@ -12,14 +12,10 @@ const createUser = async (req, res) => {
 
 const findUser = async (req, res) => {
   const user = req.body;
-  try {
-    const foundUser = await userService.findUser(user);
-    const { type, message } = foundUser;
-    if (!type) return res.status(200).json({ message });
-    return res.status(type).json({ message });
-  } catch (err) {
-    return res.status(409).json({ message: err.message });
-  }
+  const foundUser = await userService.findUser(user);
+  const { type, message } = foundUser;
+  if (!type) return res.status(200).json({ message });
+  return res.status(type).json({ message });
 };
 
 module.exports = {
