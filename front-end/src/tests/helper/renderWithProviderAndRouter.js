885bc705b62cf61c356c0fffb348053ed86e741f
André Pestana Silva <atlas.ifpa@gmail.com>
2023-05-16 19:12:11 -0300

    refactor: implementa rota corretamente com memoryHistory

--- front-end/src/tests/helper/renderWithProviderAndRouter.js
+++ front-end/src/tests/helper/renderWithProviderAndRouter.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
 import DeliveryProvider from '../../contexts/DeliveryProvider';
 
-const renderWithProviderAndRouter = (component) => ({
-  ...render(
-    <BrowserRouter>
-      <DeliveryProvider>
-        { component }
-      </DeliveryProvider>
-    </BrowserRouter>,
-  ),
-});
+const renderWithProviderAndRouter = (component) => {
+  const history = createMemoryHistory();
+  return {
+    ...render(
+      <Router history={ history }>
+        <DeliveryProvider>
+          { component }
+        </DeliveryProvider>
+      </Router>,
+    ),
+    history,
+  };
+};
 
 export default renderWithProviderAndRouter;
