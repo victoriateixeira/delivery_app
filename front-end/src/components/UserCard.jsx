8245cf59ae9221929cf0b2965eea6748d2379134
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-05-16 17:12:09 -0300

    feat: cria o componente UserCard

---
+++ front-end/src/components/UserCard.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+function UserCard({ savedUser, index, removesUser }) {
+  return (
+    <tr key={ savedUser.id }>
+      <td
+        data-testid={ `admin_manage__element-user-table-item-number-${index}` }
+      >
+        {savedUser.id}
+      </td>
+      <td
+        data-testid={ `admin_manage__element-user-table-name-${index}` }
+      >
+        {savedUser.name}
+      </td>
+      <td
+        data-testid={ `admin_manage__element-user-table-email-${index}` }
+      >
+        {savedUser.email}
+
+      </td>
+      <td
+        data-testid={ `admin_manage__element-user-table-role-${index}` }
+      >
+        {savedUser.role}
+
+      </td>
+      <td>
+        <button
+          type="button"
+          data-testid={ `admin_manage__element-user-table-remove-${index}` }
+          onClick={ () => removesUser }
+        >
+          Excluir
+        </button>
+      </td>
+    </tr>
+  );
+}
+UserCard.propTypes = {
+  savedUser: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    role: PropTypes.string.isRequired,
+  }).isRequired,
+  index: PropTypes.number.isRequired,
+  removesUser: PropTypes.func.isRequired,
+};
--- front-end/src/pages/Admin.jsx
+++ front-end/src/pages/Admin.jsx
@@ -14,6 +14,7 @@ function Admin() {
     event.preventDefault();
     setUserList([...userList, newUser]);
     setNewUser('');
+    // adicionar lógica para fazer a requisição POST
   };
   isRegisterButtonDisabled = () => {
     const { sellerName, email, password, role } = newUser;
@@ -24,11 +25,18 @@ function Admin() {
 
     return isSellerName && isEmail && isPassword && isRole;
   };
+
+  removesUser = (user) => {
+    const updatedUserList = userList.filter((u) => u.id !== user.id);
+    setUserList(updatedUserList);
+    // adicionar lógica para deletar do banco de dados
+  };
   return (
     <>
       <NavBar />
 
       <div>
+        <h1>Cadastrar novo usuário</h1>
         <form>
           <label htmlFor="name">
             Nome
@@ -86,6 +94,27 @@ function Admin() {
             Cadastrar
           </button>
         </form>
+      </div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th>Nome</th>
+              <th>E-mail</th>
+              <th>Tipo</th>
+              <th>Excluir</th>
+            </tr>
+          </thead>
+          <tbody>
+            {savedUsers.length > 0
+            && savedUsers.map((user, index) => (
+              <tr key={ user.id }>
+                <UserCard user={ user } index={ index } removesUser={ removesUser } />
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </>
   );
