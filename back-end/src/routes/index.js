012e2a5749f21e0674c88e835d1f16f21e24a99b
André Pestana Silva <atlas.ifpa@gmail.com>
2023-05-17 12:51:59 -0300

    refactor: padroniza nome da rota de admin no routes/index

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const userRoutes = require('./user.routes');
 const ordersRoutes = require('./orders.routes');
 const productRoutes = require('./products.routes');
-const adminRouter = require('./admin.routes');
+const adminRoutes = require('./admin.routes');
 
 const router = express.Router();
 
 router.use('/user', userRoutes);
-router.use('/admin', adminRouter);
+router.use('/admin', adminRoutes);
 router.use('/customers/orders', ordersRoutes);
 router.use('/customer/orders', ordersRoutes);
 router.use('/customer/products', productRoutes);
